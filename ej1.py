6e35b1c8e063a907aa6365d646682693a97cf717
Juan Pablo <juanpablo3705@gmail.com>
2026-07-07 18:47:34 -0400

    ej1 avanzado, faltan opciones  4 y 5

--- ej1.py
+++ ej1.py
@@ -21,15 +21,41 @@ def leer_opcion():
         except ValueError:
             print("Error, la opción debe ser un número entero.")
 
-# funcion opcion 1 unidades por tipo de arreglo:
+# funcion opcion 1 - unidades por tipo de arreglo:
 def unidades_tipo(tipo, arreglos, bodega):
-    while True:
-        tipo = input("Ingrese tipo de arreglo (ramo, caja o centro): ").lower().strip()
-        if tipo != "ramo" or tipo != "caja" or tipo != "centro":
-            print("Error, debe ingresar un tipo de arreglo válido (ramo, caja o centro).")
-        else:
-            break
-    
+    contador_unidades = 0
+    for clave, valores in arreglos.items():
+        if valores[1] == tipo:
+            contador_unidades = contador_unidades + bodega[clave][1]
+    print(f"El total de arreglos correspondientes es de {contador_unidades}.")
+
+# funcion opcion 2 - buqueda de arreglos por rango de precio:
+def busqueda_precio(p_min, p_max, arreglos, bodega):
+    lista_encontrados = []
+    for clave, valores in bodega.items():
+        if (valores[0] >= p_min) and (valores[0] <= p_max) and (valores[1] > 0):
+            nombre = arreglos[clave][0]
+            codigo = clave
+            lista_encontrados.append(f"{nombre}--{codigo}")
+    if len(lista_encontrados) == 0:
+        print("Error, no hay arreglos en ese rango de precios.")
+    else:
+        lista_encontrados.sort()
+        print(f"Los arreglos encontrados son: {lista_encontrados}.")
+
+# funcion opcion 3 - buscar codigo:
+def buscar_codigo(codigo, bodega):
+    return codigo in bodega
+
+# funcion opcion 3 - actualizar precio:
+def actualizar_precio(codigo, nuevo_precio, bodega):
+    if buscar_codigo(codigo, bodega):
+        bodega[codigo][0] = nuevo_precio
+        return True
+    else:
+        return False
+
+
 
 
 
@@ -55,11 +81,66 @@ while True:
     opcion_elegida = leer_opcion()
     match opcion_elegida:
         case 1:
-            print("1")
+            if len(arreglos) == 0:
+                print("Error, no hay arreglos registrados.")
+            else:
+                while True:
+                    tipo = input("Ingrese tipo de arreglo: ").lower().strip()
+                    if tipo != "":
+                        break
+                    else:
+                        print("Error, debe ingresar un tipo de arreglo.")
+                unidades_tipo(tipo, arreglos, bodega)
         case 2:
-            print("2")
+            if len(arreglos) == 0:
+                print("Error, no hay arreglos registrados.")
+            else:
+                while True:
+                    try:
+                        p_min = int(input("Ingrese un precio mínimo para el rango a buscar: "))
+                        if p_min >= 0:
+                            break
+                        else:
+                            print("Error, el precio mínimo debe ser mayor o igual que cero.")
+                    except ValueError:
+                        print("Debe ingresar valores enteros.")
+                while True:
+                    try:
+                        p_max = int(input("Ingrese un precio máximo para el rango a buscar: "))
+                        if (p_max >= p_min) and (p_max) > 0:
+                            break
+                        else:
+                            print("Error, el precio máximo debe ser mayor que el precio mínimo y mayor que cero.")
+                    except ValueError:
+                        print("Debe ingresar valores enteros.")
+                busqueda_precio(p_min, p_max, arreglos, bodega)
         case 3:
-            print("3")
+            if len(bodega) == 0:
+                print("Error, no hay arreglos registrados.")
+            else:
+                while True:
+                    while True:
+                        codigo = input("Ingrese el código del arreglo a actualizar: ").strip().upper()
+                        if buscar_codigo(codigo, bodega) == False:
+                            print("Error, el código no existe.")
+                        else:
+                            break
+                    while True:
+                        try:
+                            nuevo_precio = float(input("Ingrese el nuevo precio: "))
+                            if nuevo_precio <= 0:
+                                print("Error, el nuevo precio debe ser mayor o igual que cero.")
+                            else:
+                                break
+                        except ValueError:
+                            print("Error, el nuevo precio debe ser un número entero o decimal.")
+                    if actualizar_precio(codigo, nuevo_precio, bodega):
+                        print("Precio actualizado con éxito.")
+                    else:
+                        print("Error, código no encontrado.")
+                    otro_intento = input("¿Desea actualizar otro precio? (s/n)").lower()
+                    if otro_intento != "s":
+                        break
         case 4:
             print("4")
         case 5:
